2cc03e491b4b22077ecdfee1851d6a223a96537d
Felix Gabel <felix.gabel@me.com>
2013-12-01 16:38:50 +0100

    Comments added to XUITableViewController
    Signed-off-by: Felix Gabel <felix.gabel@me.com>

--- Source/XUITableViewController.m
+++ Source/XUITableViewController.m
@@ -27,7 +27,11 @@
 
 - (void)viewDidLoad {
 	[super viewDidLoad];
+	
+	//we need to disable automatic deselection for our custom deselection behavior
 	[super setClearsSelectionOnViewWillAppear:NO];
+	
+	//my prefered seperator style
 	[self.tableView setSeparatorStyle:UITableViewCellSeparatorStyleNone];
 	[self.tableView setBackgroundColor:[UIColor whiteColor]];
 }
@@ -39,13 +43,16 @@
 	if (![self isMovingToParentViewController] && [self clearsSelectionOnViewWillAppear]) {
 		id<UIViewControllerTransitionCoordinator> coordinator = [self transitionCoordinator];
 		NSIndexPath *indexPath = [self.tableView indexPathForSelectedRow];
-		UITableViewCell *cell = [self.tableView cellForRowAtIndexPath:indexPath];
 		
 		if ([coordinator initiallyInteractive]) {
+			
+			//fading the out the selected cells background color because a custom selectedBackgroundView will fade out after -viewDidAppear or will never trigger when the transition is interactive
 			[coordinator animateAlongsideTransition:^(id<UIViewControllerTransitionCoordinatorContext> context) {
+				UITableViewCell *cell = [self.tableView cellForRowAtIndexPath:indexPath];
 				[cell.selectedBackgroundView setAlpha:0.0];
 			} completion:nil];
 			
+			//-deselectRowAtIndexPath:animated: triggers unhighlighting of a cells labels with a timer when animated. To prevent unhighlighting in the middle of an interactive transaction we need to deselect the cell manually after the transaction is completed
 			[coordinator notifyWhenInteractionEndsUsingBlock:^(id<UIViewControllerTransitionCoordinatorContext> context) {
 				if (![context isCancelled]) {
 					[self.tableView deselectRowAtIndexPath:indexPath animated:NO];
@@ -53,6 +60,7 @@
 			}];
 			
 		} else {
+			//default behaviour for pressing the back-button
 			[self.tableView deselectRowAtIndexPath:indexPath animated:YES];
 		}
 	}
