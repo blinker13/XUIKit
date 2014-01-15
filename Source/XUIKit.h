3790c7ad6d2deda7f7b8b90a7c814d69808f79f8
Felix Gabel <felix.gabel@me.com>
2014-01-15 20:35:24 +0100

    imports sorted
    Signed-off-by: Felix Gabel <felix.gabel@me.com>

--- Source/XUIKit.h
+++ Source/XUIKit.h
@@ -6,20 +6,29 @@
 //  Copyright (c) 2013 Felix Gabel. All rights reserved.
 //
 
+// Views
 #import <XUIKit/XUILabel.h>
 #import <XUIKit/XUIButton.h>
 
+
+// Text View
+#import <XUIKit/XUITextStorage.h>
+#import <XUIKit/XUITextViewController.h>
+#import <XUIKit/XUITextView.h>
+
+#import <XUIKit/NSMutableAttributedString+XUIKit.h>
+#import <XUIKit/NSTextStorage+XUIKit.h>
+#import <XUIKit/UITextView+XUIKit.h>
+
+
+// Table View
 #import <XUIKit/XUITableViewController.h>
 #import <XUIKit/XUITableViewCell.h>
 
-#import <XUIKit/XUITextViewController.h>
-#import <XUIKit/XUITextView.h>
-#import <XUIKit/XUITextStorage.h>
+#import <XUIKit/UITableViewCell+XUIKit.h>
 
-#import <XUIKit/NSTextStorage+XUIKit.h>
-#import <XUIKit/NSMutableAttributedString+XUIKit.h>
 
+// Categories
 #import <XUIKit/UIViewController+XUIKit.h>
 #import <XUIKit/UIBarButtonItem+XUIKit.h>
-#import <XUIKit/UITableViewCell+XUIKit.h>
-#import <XUIKit/UITextView+XUIKit.h>
+#import <XUIKit/UIView+XUIKit.h>
