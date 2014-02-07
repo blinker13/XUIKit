e832d466a2747dc9fd04a0304564229ae243f957
Felix Gabel <felix.gabel@me.com>
2014-02-07 18:08:45 +0100

    unused code removed
    Signed-off-by: Felix Gabel <felix.gabel@me.com>

--- Source/XUITableViewCell.m
+++ Source/XUITableViewCell.m
@@ -13,12 +13,12 @@
 
 - (void)setHighlighted:(BOOL)highlighted animated:(BOOL)animated {
 	[super setHighlighted:highlighted animated:animated];
-	[self setCustomViewsHighlighted:highlighted];
-}
-
-- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
-	[super setSelected:selected animated:animated];
-	[self setCustomViewsHighlighted:selected];
+	
+	for (id view in self.contentView.subviews) {
+		if ([view respondsToSelector:@selector(setHighlighted:)]) {
+			[view setHighlighted:highlighted];
+		}
+	}
 }
 
 
@@ -32,17 +32,6 @@
 	UIView *selectionView = [[UIView alloc] initWithFrame:CGRectZero];
 	[selectionView setBackgroundColor:selectedBackgroundColor];
 	[self setSelectedBackgroundView:selectionView];
-}
-
-
-#pragma mark - private methods
-
-- (void)setCustomViewsHighlighted:(BOOL)highlighted {
-	for (id view in self.contentView.subviews) {
-		if ([view respondsToSelector:@selector(setHighlighted:)]) {
-			[view setHighlighted:highlighted];
-		}
-	}
 }
 
 @end
