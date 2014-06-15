7dd0551830a5e50693d08f93e4fa288487bcf6b7
Felix Gabel <felix.gabel@me.com>
2014-06-15 22:22:42 +0200

    Targets changed
    Signed-off-by: Felix Gabel <felix.gabel@me.com>

--- Tests/XUIKitTests.m
+++ Tests/XUIKitTests.m
@@ -2,11 +2,11 @@
 //  XUIKitTests.m
 //  XUIKitTests
 //
-//  Created by Felix Gabel on 21/11/13.
-//  Copyright (c) 2013 Felix Gabel. All rights reserved.
+//  Created by Felix Gabel on 15/06/14.
+//  Copyright (c) 2014 Felix Gabel. All rights reserved.
 //
 
-@import XCTest;
+#import <XCTest/XCTest.h>
 
 @interface XUIKitTests : XCTestCase
 
@@ -14,21 +14,26 @@
 
 @implementation XUIKitTests
 
-- (void)setUp
-{
+- (void)setUp {
     [super setUp];
     // Put setup code here. This method is called before the invocation of each test method in the class.
 }
 
-- (void)tearDown
-{
+- (void)tearDown {
     // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
 }
 
-- (void)testExample
-{
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
 }
 
 @end
